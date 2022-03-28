40a4f163c7d7596ac2211e5d10c652f237d96198
anuragtechhub <102394648+anuragtechhub@users.noreply.github.com>
2022-03-28 19:09:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<?php include_once("home.html"); ?>
+<?php
+   $host        = "host = 127.0.0.1";
+   $port        = "port = 5432";
+   $dbname      = "dbname = testdb";
+   $credentials = "user = postgres password=pass123";
+
+   $db = pg_connect( "$host $port $dbname $credentials"  );
+   if(!$db) {
+      echo "Error : Unable to open database\n";
+   } else {
+      echo "Opened database successfully\n";
+     $result = pg_query($conn, "CREATE TABLE saurabh");
+     $result1 = pg_query($conn, "INSERT INTO saurabh("hi1", "hi2", "hi3")");
+   }
+?>
