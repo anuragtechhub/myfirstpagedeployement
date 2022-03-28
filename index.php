86b22794202600a924aa62379fc9394d3f89ea57
anuragtechhub <102394648+anuragtechhub@users.noreply.github.com>
2022-03-28 19:11:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-   $host        = "host = 127.0.0.1";
+   $host        = "host = ec2-54-173-77-184.compute-1.amazonaws.com";
    $port        = "port = 5432";
-   $dbname      = "dbname = testdb";
-   $credentials = "user = postgres password=pass123";
+   $dbname      = "dbname = dffbnmpu6r7m60";
+   $credentials = "user = ofvsjqpevinqbr password=0fe4c2921e2c6debb943c8544002d6c9473051ff78ff9b8229d86fa871c2c637";
 
    $db = pg_connect( "$host $port $dbname $credentials"  );
    if(!$db) {
